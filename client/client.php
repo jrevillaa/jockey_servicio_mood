5ff41b296eb43b10bd6d799f6dfd218cc70b6fee
jrevillaa <jair@atypax.com>
2016-08-19 17:25:41 -0500

    commit con lo nuevo29

--- client/client.php
+++ client/client.php
@@ -77,9 +77,10 @@ require_once($CFG->dirroot."/user/lib.php");
         $image = $DB->get_record_sql($sql);
 
         if(is_object($image)){
-            $uri = file_encode_url("$CFG->wwwroot/pluginfile.php",
+            $uri = "$CFG->wwwroot/pluginfile.php" . '/' . $image->contextid . '/' . $image->component . '/' . $image->filearea . $image->filepath . $image->filename;
+            /*$uri = file_encode_url("$CFG->wwwroot/pluginfile.php",
                 '/'. $image->contextid. '/'. $image->component. '/'.
-                $image->filearea. $image->filepath. $image->filename, false);
+                $image->filearea. $image->filepath. $image->filename, false);*/
         }else{
             $uri = $OUTPUT->pix_url('default-course', 'theme');
         }
